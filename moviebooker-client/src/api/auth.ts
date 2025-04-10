346c7fdef5639622447f5d31546becd7a471e76b
Ga1aad <136579419+Ga1aad@users.noreply.github.com>
2025-04-10 12:11:00 +0200

    maj front avant deploiement

--- moviebooker-client/src/api/auth.ts
+++ moviebooker-client/src/api/auth.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 import { jwtDecode } from "jwt-decode";
 
-const API_URL = "http://localhost:3000";
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
 
 // Créer une instance axios avec la configuration de base
 export const api = axios.create({
